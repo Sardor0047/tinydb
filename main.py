556500d0feba7393886dd4f8019490cb1078dbd7
Sardor0047 <macbookpro@MacBook-Pro-MacBook.local>
2025-01-25 19:52:49 +0500

    add 2 user

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 from tinydb import TinyDB
 db = TinyDB('user.json')
+
+db.insert({'first_name':'Otabek','last_name':'Abdurasulov','job':'Student'})
+db.insert({'first_name':'Sardor','last_name':'Muminjonov','job':'Student'})
