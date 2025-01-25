6f060f202d3850e5426696fd0f06e346278f7255
Sardor0047 <macbookpro@MacBook-Pro-MacBook.local>
2025-01-25 20:27:11 +0500

    created random_user.json and get random names

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from tinydb import TinyDB
+import requests
 db = TinyDB('user.json')
 
-db.insert({'first_name':'Otabek','last_name':'Abdurasulov','job':'Student'})
-db.insert({'first_name':'Sardor','last_name':'Muminjonov','job':'Student'})
+db_random =TinyDB('random_user.json')
+
+
+url = "https://randommer.io/api/Name?nameType=firstname&quantity=5"
+
+headers = {
+    "X-Api-Key": 'b9684522e39c47ab9cd0a655122028a5' 
+}
+response = requests.get(url,headers=headers)
+r = response.json()
+for user in r:
+    db_random.insert({'name':f"{user}"})
