c989b35a439a1d5c2a0354777a8b308dba628ecb
Sardor0047 <macbookpro@MacBook-Pro-MacBook.local>
2025-01-26 11:28:45 +0500

    created online shop

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
 from tinydb import TinyDB
 import requests
-db = TinyDB('user.json')
-
-db_random =TinyDB('random_user.json')
 
 
-url = "https://randommer.io/api/Name?nameType=firstname&quantity=5"
-
-headers = {
-    "X-Api-Key": 'b9684522e39c47ab9cd0a655122028a5' 
-}
-response = requests.get(url,headers=headers)
-r = response.json()
-for user in r:
-    db_random.insert({'name':f"{user}"})
+db = TinyDB('texnomart.json')
