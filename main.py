e02b12a2100c7cfc078b90fda17ae1f817486a18
Sardor0047 <macbookpro@MacBook-Pro-MacBook.local>
2025-01-26 12:29:44 +0500

    added Legion 5 Pro (16-inch)

--- main.py
+++ main.py
@@ -19,15 +19,27 @@ notebooks = {
             }
         },
         {
-            'model':'Yoga 9i',
+            'model':'Yoga 9i ',
             'characteristic':{
                 'Processor': 'Intel Core i7-1360P',
                 'RAM': '16 GB',
-                'Storage' : '1 TB',
-                'Display':"14' 4K UHD OLED",
+                'Storage' : '1 TB SSD',
+                'Display': "14' 4K UHD OLED",
                 'Weight':'3.09 lbs',
                 'Best For':'Creatives and multimedia',
                 'Cost':'1600-2000 $'
+            }
+        }
+        {
+            'model':'Legion 5 Pro (16-inch)',
+            'characteristic':{
+                'Processor' : 'AMD Ryzen 7 6800H',
+                'RAM' : '16 GB',
+                'Storage' : '1TB SSD',
+                'Display' : "16' WQXGA (2560x1600), IPS, 165Hz, 500 nits",
+                'Weight':'5.4 lbs',
+                'Best For':'Gamers and content creators',
+                'Cost':'1800-2300 $'
             }
         }
     ]
