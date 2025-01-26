a46af1a67afa794442f5b3bf34a58310af9fc884
Sardor0047 <macbookpro@MacBook-Pro-MacBook.local>
2025-01-26 12:15:33 +0500

    added thinkpad x1

--- main.py
+++ main.py
@@ -3,3 +3,21 @@ import requests
 
 
 db = TinyDB('texnomart.json')
+
+notebooks = {
+    'Lenovo':[
+        {
+            'model': 'ThinkPad X1 Carbon Gen 10',
+            'characteristic' : {
+                'Processor': 'Intel Core i7-1260P',
+                'RAM': '16GB',
+                'Storage': '512GB SSD',
+                'Display': "14-inch WUXGA (1920x1200)",
+                'Weight': '2.48 lbs',
+                'Best For': 'Business professionals',
+                'Cost': '1500-1800 $'
+            }
+        },
+        
+    ]
+}
